07b48b3d2089b0ad2cf4d96398eeca4f7673ffff
Nelly <nellyobijiaku@gmail.com>
2024-05-05 12:47:43 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React from "react";
 import './App.css';
 
 function App() {
-  return <div className="App">Weather App </div>;
+  return <div className="App">
+    <div className="container">
+
+
+   <h1> Weather App </h1>
+    <footer>
+      This project was coded by <a href="https://github.com/nellyobi"
+      target="_blank"
+      rel="noreferrer">
+       Nelly Obi </a> and is  
+    <a href="https://github.com/nellyobi/final-weather-app" 
+    target="_blank"
+    rel="noreferrer"> open-sourced on Github </a>
+    </footer>
+    </div>
+      </div>;
 }
 
 export default App;
