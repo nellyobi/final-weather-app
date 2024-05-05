a89a91826735cb96cf72d49f752303ceaa4e408e
Nelly <nellyobijiaku@gmail.com>
2024-05-05 20:02:45 +0100

    styled the page and added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,27 @@
 import React from "react";
 import "./Weather.css"
+import axios from "axios";
 export default function Weather() {
+    const apiKey = "c5e3a245dac7782f3e1ab46737af1838";
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
     return (
     <div className="Weather"> 
     <form>
         <div className="row">
             <div className="col-9">
-                <input type="search" placeholder="Enter a city..."
-                className="form-control"/>
+                <input 
+                type="search" 
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+                />
+                </div>
             <div className="col-3">
-                <input type="submit" 
+                <input 
+                type="submit" 
                 value="Search"
-                className="btn btn-primary" /> 
-        </div>
+                className="btn btn-primary w-100" 
+                /> 
         </div>  
         </div>
     </form>
@@ -23,11 +32,16 @@ export default function Weather() {
     </ul>
     <div className="row">
         <div className="col-6"> 
+        <div className="clearfix">
         <img
             src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" 
             alt="Cloudy"
+            className="float-left"
             />
-            6 ℃
+            <span className="temperature">6</span>
+            <span className="unit">℃</span>
+            
+        </div>
         </div>
         <div className="col-6">
             <ul>
